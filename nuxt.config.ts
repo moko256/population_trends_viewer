52f034b88e1d4856c603394977b8d08fe76746d5
moko256 <koutaro.mo@gmail.com>
2024-06-03 08:49:08 +0900

    chore: setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   runtimeConfig: {
     resasApiKey: process.env.RESAS_API_KEY,
+  },
+
+  modules: ["@nuxt/eslint"],
+
+  // https://eslint.nuxt.com/packages/module
+  eslint: {
+    config: {
+      stylistic: true
+    }
   }
 })
