7a034c60bba92a99e4dc3809798efa386cc024a0
moko256 <koutaro.mo@gmail.com>
2024-06-05 23:49:24 +0900

    feat: add state and connect state to page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,64 @@
 <template>
   <div>
-    <p>{{ text }}</p>
-    <button @click="countUp">Count up!</button>
+    <!-- Prefectures -->
+    <div>
+      <div v-if="isPrefecturesLoading">
+        <p>Loading prefectures...</p>
+      </div>
+      <div v-if="isPrefecturesError">
+        <p>Error when loading prefectures.</p>
+        <button @click="loadPrefectures">Retry</button>
+      </div>
+      <li v-for="{ name, code } in prefectures">
+        <input
+          type="checkbox"
+          :value="name"
+          @change="onPrefectureCheck(code, $event)"
+          :checked="isPrefectureSelected[code] == true"
+        />
+        <label :for="name">{{ name }}</label>
+      </li>
+      <p>{{ JSON.stringify(prefectures) }}</p>
+    </div>
+
+    <!-- Populations -->
+    <div>
+      <div v-if="isPopulationsLoading">
+        <p>Loading populations...</p>
+      </div>
+      <div v-if="isPopulationsError">
+        <p>Error when loading populations.</p>
+        <button @click="loadPopulations">Retry</button>
+      </div>
+      <p>{{ JSON.stringify(populations) }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useCounterState } from "~/store/counter";
+import { useAppState } from "~/store/app_state";
 
-const store = useCounterState();
-const text = computed(() => store.displayText);
+const store = useAppState();
 
-function countUp() {
-  console.log("Hello!");
-  store.increment();
+const {
+  prefectures,
+  isPrefecturesLoading,
+  isPrefecturesError,
+  isPrefectureSelected,
+  populations,
+  isPopulationsLoading,
+  isPopulationsError,
+} = storeToRefs(store);
+
+const { loadPrefectures, loadPopulations, selectPrefecture } = store;
+
+onMounted(() => {
+  loadPrefectures();
+});
+
+function onPrefectureCheck(code: PrefectureCode, event: Event) {
+  const checked = (event.target as HTMLInputElement).checked;
+  selectPrefecture(code, checked);
 }
 </script>
 
---
+++ store/app_state.ts
@@ -0,0 +1,74 @@
+import { defineStore } from "pinia";
+import { PopulationsRepo } from "~/client_domain/repository/repo_populations";
+import { PrefecturesRepo } from "~/client_domain/repository/repo_prefectures";
+
+export const useAppState = defineStore("counter", () => {
+  const prefecturesRepo = inject<PrefecturesRepo>(PrefecturesRepo.Key)!;
+  const populationsRepo = inject<PopulationsRepo>(PopulationsRepo.Key)!;
+
+  const prefectures = ref<Prefectures>([]);
+  const isPrefecturesLoading = ref(true);
+  const isPrefecturesError = ref(false);
+
+  const isPrefectureSelected = ref<Record<PrefectureCode, boolean>>({});
+  const selectedPrefectureCodes = computed(() =>
+    prefectures.value
+      .filter((v) => isPrefectureSelected.value[v.code] == true)
+      .map((v) => v.code),
+  );
+
+  const populations = ref<PopulationCompositions>([]);
+  const isPopulationsLoading = ref(true);
+  const isPopulationsError = ref(false);
+
+  async function loadPrefectures() {
+    isPrefecturesLoading.value = true;
+    isPrefecturesError.value = false;
+    isPrefectureSelected.value = {};
+
+    try {
+      prefectures.value = await prefecturesRepo.getPrefectures();
+    } catch (e) {
+      console.log(e);
+      isPrefecturesError.value = true;
+    }
+
+    isPrefecturesLoading.value = false;
+  }
+
+  async function loadPopulations() {
+    isPopulationsLoading.value = true;
+    isPopulationsError.value = false;
+
+    try {
+      populations.value = await populationsRepo.getPopulationsByPrefecture(
+        selectedPrefectureCodes.value,
+      );
+    } catch (e) {
+      console.log(e);
+      isPopulationsError.value = true;
+    }
+
+    isPopulationsLoading.value = false;
+  }
+
+  function selectPrefecture(prefCode: PrefectureCode, selected: boolean) {
+    isPrefectureSelected.value[prefCode] = selected;
+
+    loadPopulations();
+  }
+
+  return {
+    prefectures,
+    isPrefecturesLoading,
+    isPrefecturesError,
+    isPrefectureSelected,
+    selectedPrefectureCodes,
+    populations,
+    isPopulationsLoading,
+    isPopulationsError,
+    loadPrefectures,
+    loadPopulations,
+    selectPrefecture,
+  };
+});
